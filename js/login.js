bc581c9b53b36dab35aca920103a42c41ed3cc2e
kromate <akpananthony33@gmail.com>
2020-06-12 14:16:23 +0100

    firebase2

---
+++ js/login.js
@@ -0,0 +1,6 @@
+document.getElementById('submit').addEventListener('click', function (e) {
+  e.preventDefault()
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+  
+})
