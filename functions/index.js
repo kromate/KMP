1a18f901fb68f1c92b2fc324aa58298068595a53
kromate <akpananthony33@gmail.com>
2020-06-12 13:56:42 +0100

    firebase

---
+++ functions/index.js
@@ -0,0 +1,27 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
+
+// auth trigger (new user signup)
+exports.newUserSignUp = functions.auth.user().onCreate(user => {
+  // for background triggers you must return a value/promise
+  return admin.firestore().collection('users').doc(user.uid).set({
+    ment: '',
+    mentNum:'',
+    ment2: '',
+    mentNum2:'',
+    ment2: '',
+    mentNum2:'',
+    email: user.email,
+    phone: '',
+
+  });
+});
+
+// auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete(user => {
+  const doc = admin.firestore().collection('users').doc(user.uid);
+  return doc.delete();
+});
---
+++ js/fireconfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBSGHdOapiC2xzQp6NvVlhab_iT9ho2YfM",
+  authDomain: "k-m-p-b7852.firebaseapp.com",
+  databaseURL: "https://k-m-p-b7852.firebaseio.com",
+  projectId: "k-m-p-b7852",
+  storageBucket: "k-m-p-b7852.appspot.com",
+  messagingSenderId: "620441081589",
+  appId: "1:620441081589:web:6693b241346795f5c217bc",
+  measurementId: "G-5XM9BV782G"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
