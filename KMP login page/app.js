eabb6a9d71812a66a90aaf51bc1b93a0f7bf158a
Harith Onigemo <45686269+Harithmetic1@users.noreply.github.com>
2020-06-14 20:22:04 +0100

    login page

---
+++ KMP login page/app.js
@@ -0,0 +1,10 @@
+
+function changeBg(){
+    document.body.style.backgroundColor = "purple";
+    document.getElementById('kmpp').style.color = "white";
+}
+
+function changeBgwhite(){
+    document.body.style.backgroundColor = "transparent";
+    document.getElementById('kmpp').style.color = "black";
+}
